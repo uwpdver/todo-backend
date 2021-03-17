46c4a858394b24a8ca0cc378b452f0be9f600781
liuzhihao <1119548217@qq.com>
2021-03-17 23:25:30 +0800

    feat: 增加解决跨域问题的中间件;

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 const Note = require('./models/note')
