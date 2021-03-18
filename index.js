08c31092084157aa8740874cff303599e4d3f9fb
liuzhihao <1119548217@qq.com>
2021-03-18 22:36:29 +0800

    feat: 使用统一的错误处理中间件, 修改缩进风格;

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
+
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
-
+const errorHandler = require('./middleware').errorHandler
 const app = express();
 
+app.use(express.static('build'))
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
-app.use(express.static('build'))
+app.use(errorHandler);
 
 const Note = require('./models').Note;
 
@@ -17,15 +19,8 @@ const Note = require('./models').Note;
 app.get('/api/items', (request, response) => {
   const { keyword = '' } = request.query
   Note.find({ content: { $regex: keyword } })
-    .then((result) => {
-      return response.json({
-        message: 'success',
-        data: result
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    .then((result) => response.json({ message: 'success', data: result }))
+    .catch(error => next(error))
 })
 
 // 查询单个条目
@@ -33,14 +28,13 @@ app.get('/api/items/:id', (request, response) => {
   const { id } = request.params
   Note.findById(id)
     .then((result) => {
-      return response.json({
-        message: 'success',
-        data: result
-      })
+      if (result) {
+        response.json({ message: 'success', data: result })
+      } else {
+        response.status(404).end();
+      }
     })
-    .catch(err => {
-      console.log(err)
-    })
+    .catch(error => next(error))
 })
 
 // 创建新条目
@@ -56,15 +50,8 @@ app.post('/api/items', (request, response) => {
   })
 
   note.save()
-    .then(result => {
-      return response.json({
-        message: 'create success',
-        data: result
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    .then(result => response.json({ message: 'create success', data: result }))
+    .catch(error => next(error))
 })
 
 // 修改重要性
@@ -72,15 +59,8 @@ app.patch('/api/items/:id/important', (request, response) => {
   const { important } = request.body
   const { id } = request.params
   Note.findByIdAndUpdate(id, { important: important })
-    .then((result) => {
-      return response.json({
-        message: 'update success',
-        data: result
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    .then(result => response.json({ message: 'update success', data: result }))
+    .catch(error => next(error))
 })
 
 // 修改完成状态
@@ -88,33 +68,20 @@ app.patch('/api/items/:id/isCompleted', (request, response) => {
   const { isCompleted } = request.body
   const { id } = request.params
   Note.findByIdAndUpdate(id, { isCompleted: isCompleted })
-    .then((result) => {
-      return response.json({
-        message: 'update success',
-        data: result
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    .then(result => response.json({ message: 'update success', data: result }))
+    .catch(error => next(error))
 })
 
 // 删除条目
 app.delete('/api/items/:id', (request, response) => {
   const { id } = request.params
   Note.findByIdAndDelete(id)
-    .then((result) => {
-      return response.json({
-        message: 'delete success',
-        data: result
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    .then(result => response.json({ message: 'delete success', data: result }))
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
+
 
 app.listen(PORT, () => {
   console.log(`Todo Server running on port ${PORT}`)
