d5e62384ab60b7fa2983764d3442e06229dce17a
liuzhihao <1119548217@qq.com>
2021-03-18 22:44:16 +0800

    feat: 增加错误处理中间件中对数据库字段验证错误的处理;

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,6 +3,8 @@ const errorHandler = (error, request, response, next) => {
   
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
+  }else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
   
   next();
